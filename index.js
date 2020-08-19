51e9956d1f4ea063dee63f4a58cf88d6ae3fdb48
Bubblebyb <bubblebyb@gmail.com>
2020-08-19 13:24:24 -0400

    v0.02
    Added ability to choose difficulty and category, better string editing.

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const stringSimilarity = require('string-similarity');
 const Discord = require('discord.js');
 const prefix = "!";
 
-let historyData = fs.readFileSync('./data/history.json');
-let history = JSON.parse(historyData);
-console.log('History Questions Loaded');
+let questionsData = fs.readFileSync('./data/questions.json');
+let questions = JSON.parse(questionsData);
+console.log('Questions Loaded');
 let userFile = fs.readFileSync('./data/users.json');
 let userData = JSON.parse(userFile);
 console.log('User Data Loaded');
+let userQuestionsData = fs.readFileSync('./data/userQuestions.json');
+let userQuestions = JSON.parse(userQuestionsData);
+console.log('User Questions Loaded')
 
 function reloadData() {
     userFile = fs.readFileSync('./data/users.json');
@@ -36,57 +39,65 @@ client.on('message', message => {
         userData[message.author.id].playing = "no";
         fs.writeFileSync('./data/users.json', JSON.stringify(userData));
         reloadData();
-        console.log(message.author.id);
     }
 
-    if (!userData[message.author.id]) {
+    function initUserQuestionData() {
+        userQuestions[message.author.id] = {};
+        userQuestions[message.author.id].bonuses = [];
+        userQuestions[message.author.id].bonusesTemp = [];
+        fs.writeFileSync('./data/userQuestions.json', JSON.stringify(userQuestions));
+    }
+
+    if (!userData[message.author.id] && !message.author.bot) {
         initUserData();
+    }
+    if (!userQuestions[message.author.id] && !message.author.bot) {
+        initUserQuestionData();
+    }
+
+    function replaceStrings(n) {
+        userQuestions[message.author.id].bonuses[n].formatted_answers[0] = userQuestions[message.author.id].bonuses[n].formatted_answers[0].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].formatted_answers[1] = userQuestions[message.author.id].bonuses[n].formatted_answers[1].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].formatted_answers[2] = userQuestions[message.author.id].bonuses[n].formatted_answers[2].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].answers[0] = userQuestions[message.author.id].bonuses[n].answers[0].replace(/ *\([^)]*\) */g, ' ').replace(/ *\[[^)]* */g, ' ').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/(<([^>]+)>)/gi, "").replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].answers[1] = userQuestions[message.author.id].bonuses[n].answers[1].replace(/ *\([^)]*\) */g, ' ').replace(/ *\[[^)]* */g, ' ').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/(<([^>]+)>)/gi, "").replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].answers[2] = userQuestions[message.author.id].bonuses[n].answers[2].replace(/ *\([^)]*\) */g, ' ').replace(/ *\[[^)]* */g, ' ').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/(<([^>]+)>)/gi, "").replace(/ANSWER: /g, '');
+        userQuestions[message.author.id].bonuses[n].formatted_leadin = userQuestions[message.author.id].bonuses[n].formatted_leadin.replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*');
+        userQuestions[message.author.id].bonuses[n].formatted_texts[0] = userQuestions[message.author.id].bonuses[n].formatted_texts[0].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*');
+        userQuestions[message.author.id].bonuses[n].formatted_texts[1] = userQuestions[message.author.id].bonuses[n].formatted_texts[1].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*');
+        userQuestions[message.author.id].bonuses[n].formatted_texts[2] = userQuestions[message.author.id].bonuses[n].formatted_texts[2].replace(/<\/?strong>/g, '**').replace(/<\/?em>/g, '*');
     }
 
     function firstPart() {
-        let n = Math.floor((Math.random() * 46));
-        history.data.bonuses[n].formatted_answers[0] = history.data.bonuses[n].formatted_answers[0].replace(/<\/?strong>/g, '**');
-        history.data.bonuses[n].formatted_answers[1] = history.data.bonuses[n].formatted_answers[1].replace(/<\/?strong>/g, '**');
-        history.data.bonuses[n].formatted_answers[2] = history.data.bonuses[n].formatted_answers[2].replace(/<\/?strong>/g, '**');
-        history.data.bonuses[n].answers[0] = history.data.bonuses[n].answers[0].replace(/ *\([^)]*\) */g, ' ');
-        history.data.bonuses[n].answers[1] = history.data.bonuses[n].answers[1].replace(/ *\([^)]*\) */g, ' ');
-        history.data.bonuses[n].answers[2] = history.data.bonuses[n].answers[2].replace(/ *\([^)]*\) */g, ' ');
+        let n = Math.floor((Math.random() * (userQuestions[message.author.id].bonuses.length - 1)));
+        replaceStrings(n);
         let firstEmbed = new Discord.MessageEmbed()
             .setColor('#f5f5f5')
+            .setAuthor(userQuestions[message.author.id].bonuses[n].tournament.name)
             .setTitle('Bonus One')
-            .setDescription(history.data.bonuses[n].leadin + ' ' +
-                history.data.bonuses[n].texts[0])
+            .setDescription(userQuestions[message.author.id].bonuses[n].formatted_leadin + ' ' +
+                userQuestions[message.author.id].bonuses[n].formatted_texts[0])
             .setFooter(userData[message.author.id].points + ' points in ' + userData[message.author.id].parts + ' bonus parts');
         message.channel.send(firstEmbed);
-        console.log(history.data.bonuses[n].answers[0])
+        console.log(userQuestions[message.author.id].bonuses[n].answers[0])
         let collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
         collector.on('collect', message => {
-            if (message.content == "!end") {
+            if (message.content == prefix + 'end') {
                 collector.stop();
-                var endEmbed = new Discord.MessageEmbed()
-                    .setTitle('Pk ended')
-                    .setDescription(userData[message.author.id].points / userData[message.author.id].parts * 3)
-                message.channel.send(endEmbed);
-                userData[message.author.id] = {};
-                userData[message.author.id].points = 0;
-                userData[message.author.id].parts = 0;
-                userData[message.author.id].playing = "no";
-                fs.writeFileSync('./data/users.json', JSON.stringify(userData));
-                reloadData();
             } else {
-                console.log(stringSimilarity.compareTwoStrings(message.content.toLowerCase(), history.data.bonuses[n].answers[0].toLowerCase()));
-                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), history.data.bonuses[n].answers[0].toLowerCase()) > 0.4) {
+                console.log(stringSimilarity.compareTwoStrings(message.content.toLowerCase(), userQuestions[message.author.id].bonuses[n].answers[0].toLowerCase()));
+                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), userQuestions[message.author.id].bonuses[n].answers[0].toLowerCase()) > 0.4) {
                     var correctEmbed = new Discord.MessageEmbed()
                         .setColor('#53d645')
                         .setTitle('Correct')
-                        .setDescription(history.data.bonuses[n].formatted_answers[0])
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[0])
                     message.channel.send(correctEmbed);
                     userData[message.author.id].points += 10;
                 } else {
                     var incorrectEmbed = new Discord.MessageEmbed()
                         .setColor('#f72843')
                         .setTitle('Incorrect')
-                        .setDescription(history.data.bonuses[n].formatted_answers[0] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[0] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
                     message.channel.send(incorrectEmbed).then(embedMessage => {
                         embedMessage.react('✅');
                         embedMessage.react('❌');
@@ -111,41 +122,33 @@ client.on('message', message => {
     }
 
     function secondPart(n) {
+        replaceStrings(n);
         let secondEmbed = new Discord.MessageEmbed()
             .setColor('#f5f5f5')
+            .setAuthor(userQuestions[message.author.id].bonuses[n].tournament.name)
             .setTitle('Bonus Two')
-            .setDescription(history.data.bonuses[n].texts[1])
+            .setDescription(userQuestions[message.author.id].bonuses[n].formatted_texts[1])
             .setFooter(userData[message.author.id].points + ' points in ' + userData[message.author.id].parts + ' bonus parts');
         message.channel.send(secondEmbed);
-        console.log(history.data.bonuses[n].answers[1])
+        console.log(userQuestions[message.author.id].bonuses[n].answers[1])
         let collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
         collector.on('collect', message => {
-            if (message.content == "!end") {
+            if (message.content == prefix + 'end') {
                 collector.stop();
-                var endEmbed = new Discord.MessageEmbed()
-                    .setTitle('Pk ended')
-                    .setDescription(userData[message.author.id].points / userData[message.author.id].parts * 3)
-                message.channel.send(endEmbed);
-                userData[message.author.id] = {};
-                userData[message.author.id].points = 0;
-                userData[message.author.id].parts = 0;
-                userData[message.author.id].playing = "no";
-                fs.writeFileSync('./data/users.json', JSON.stringify(userData));
-                reloadData();
             } else {
-                console.log(stringSimilarity.compareTwoStrings(message.content, history.data.bonuses[n].answers[1].toLowerCase()));
-                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), history.data.bonuses[n].answers[1].toLowerCase()) > 0.4) {
+                console.log(stringSimilarity.compareTwoStrings(message.content, userQuestions[message.author.id].bonuses[n].answers[1].toLowerCase()));
+                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), userQuestions[message.author.id].bonuses[n].answers[1].toLowerCase()) > 0.4) {
                     var correctEmbed = new Discord.MessageEmbed()
                         .setColor('#53d645')
                         .setTitle('Correct')
-                        .setDescription(history.data.bonuses[n].formatted_answers[1])
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[1])
                     message.channel.send(correctEmbed);
                     userData[message.author.id].points += 10;
                 } else {
                     var incorrectEmbed = new Discord.MessageEmbed()
                         .setColor('#f72843')
                         .setTitle('Incorrect')
-                        .setDescription(history.data.bonuses[n].formatted_answers[1] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[1] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
                     message.channel.send(incorrectEmbed).then(embedMessage => {
                         embedMessage.react('✅');
                         embedMessage.react('❌');
@@ -170,41 +173,33 @@ client.on('message', message => {
     }
 
     function thirdPart(n) {
+        replaceStrings(n);
         let thirdEmbed = new Discord.MessageEmbed()
             .setColor('#f5f5f5')
+            .setAuthor(userQuestions[message.author.id].bonuses[n].tournament.name)
             .setTitle('Bonus Two')
-            .setDescription(history.data.bonuses[n].texts[2])
+            .setDescription(userQuestions[message.author.id].bonuses[n].formatted_texts[2])
             .setFooter(userData[message.author.id].points + ' points in ' + userData[message.author.id].parts + ' bonus parts');
         message.channel.send(thirdEmbed);
-        console.log(history.data.bonuses[n].answers[2])
+        console.log(userQuestions[message.author.id].bonuses[n].answers[2])
         let collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
         collector.on('collect', message => {
-            if (message.content == "!end") {
+            if (message.content == prefix + 'end') {
                 collector.stop();
-                var endEmbed = new Discord.MessageEmbed()
-                    .setTitle('Pk ended')
-                    .setDescription(userData[message.author.id].points / userData[message.author.id].parts * 3)
-                message.channel.send(endEmbed);
-                userData[message.author.id] = {};
-                userData[message.author.id].points = 0;
-                userData[message.author.id].parts = 0;
-                userData[message.author.id].playing = "no";
-                fs.writeFileSync('./data/users.json', JSON.stringify(userData));
-                reloadData();
             } else {
-                console.log(stringSimilarity.compareTwoStrings(message.content.toLowerCase(), history.data.bonuses[n].answers[2].toLowerCase()));
-                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), history.data.bonuses[n].answers[2].toLowerCase()) > 0.4) {
+                console.log(stringSimilarity.compareTwoStrings(message.content.toLowerCase(), userQuestions[message.author.id].bonuses[n].answers[2].toLowerCase()));
+                if (stringSimilarity.compareTwoStrings(message.content.toLowerCase(), userQuestions[message.author.id].bonuses[n].answers[2].toLowerCase()) > 0.4) {
                     var correctEmbed = new Discord.MessageEmbed()
                         .setColor('#53d645')
                         .setTitle('Correct')
-                        .setDescription(history.data.bonuses[n].formatted_answers[2])
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[2])
                     message.channel.send(correctEmbed);
                     userData[message.author.id].points += 10;
                 } else {
                     var incorrectEmbed = new Discord.MessageEmbed()
                         .setColor('#f72843')
                         .setTitle('Incorrect')
-                        .setDescription(history.data.bonuses[n].formatted_answers[2] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
+                        .setDescription(userQuestions[message.author.id].bonuses[n].formatted_answers[2] + "\n\nReact ✅ or ❌ to override the decision This can only be done once")
                     message.channel.send(incorrectEmbed).then(embedMessage => {
                         embedMessage.react('✅');
                         embedMessage.react('❌');
@@ -228,23 +223,68 @@ client.on('message', message => {
         });
     }
 
-    if (commandText == "test") {
+    if (commandText == 'params') {
+      if(userData[message.author.id].playing === 'yes') {
+        message.channel.send("You can't change parameters while inside a pk!");
+      }
+      else {
+        if (args[0] == 'reset') {
+            userQuestions[message.author.id].bonuses = [];
+            userQuestions[message.author.id].bonusesTemp = [];
+            message.channel.send('Parameters reset!');
+            fs.writeFileSync('./data/userQuestions.json', JSON.stringify(userQuestions));
+        } else if (args[0] == 'all') {
+            userQuestions[message.author.id].bonuses = questions.data.bonuses;
+            message.channel.send('Parameters set to all questions!');
+            fs.writeFileSync('./data/userQuestions.json', JSON.stringify(userQuestions));
+        } else if (args[0] == 'diff') {
+            if (!args[1]) {
+                message.channel.send('Please add a difficulty (from 1 to 2) after the command');
+            } else if (args[1]) {
+                userQuestions[message.author.id].bonuses = []
+                questions.data.bonuses.forEach(bonus => {
+                    if (bonus.tournament.difficulty_num == args[1]) {
+                        userQuestions[message.author.id].bonuses.push(bonus);
+                        fs.writeFileSync('./data/userQuestions.json', JSON.stringify(userQuestions));
+                    }
+                });
+                message.channel.send('Parameters set to difficulty ' + args[1] + '!');
+            }
+        } else if (args[0] == 'cat') {
+            if (!args[1]) {
+                message.channel.send('Please add a category after the command');
+            } else if (args[1]) {
+                if (args[1].toLowerCase() == "ce") {
+                    args[1] = "current events";
+                } else if (args[1].toLowerCase() == "fa") {
+                    args[1] = "fine arts";
+                }
+                userQuestions[message.author.id].bonuses = []
+                questions.data.bonuses.forEach(bonus => {
+                    if (bonus.category.name.toLowerCase() == args[1].toLowerCase()) {
+                        userQuestions[message.author.id].bonuses.push(bonus);
+                        fs.writeFileSync('./data/userQuestions.json', JSON.stringify(userQuestions));
+                    }
+                });
+                message.channel.send('Parameters set to category ' + args[1] + '!');
+            }
+        }
+    }
+  }
+
+    if (commandText == 'test') {
         var eembed = new Discord.MessageEmbed()
             .setDescription('**' + 'hit' + '**')
         message.channel.send(eembed);
     }
 
     if (commandText == "pk") {
-        if (!userData[message.author.id]) {
-            message.channel.send('This must be your first message in the server. Please try using `!pk` again!');
-        } else {
-            if (userData[message.author.id].playing === "yes") {
-                message.channel.send("You're already in a pk!");
-            } else if (userData[message.author.id].playing === "no") {
-                userData[message.author.id].playing = "yes";
-                fs.writeFileSync('./data/users.json', JSON.stringify(userData));
-                firstPart();
-            }
+        if (userData[message.author.id].playing === "yes") {
+            message.channel.send("You're already in a pk!");
+        } else if (userData[message.author.id].playing === "no") {
+            userData[message.author.id].playing = "yes";
+            fs.writeFileSync('./data/users.json', JSON.stringify(userData));
+            firstPart();
         }
     }
 
@@ -254,7 +294,6 @@ client.on('message', message => {
                 .setTitle('Pk ended')
                 .setDescription(userData[message.author.id].points / userData[message.author.id].parts * 3 + ' ppg')
             message.channel.send(endEmbed);
-            userData[message.author.id] = {};
             userData[message.author.id].points = 0;
             userData[message.author.id].parts = 0;
             userData[message.author.id].playing = "no";
