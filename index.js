36bae02cf15a58f877bac7eea471201a454e5567
Bubblebyb <bubblebyb@gmail.com>
2020-08-29 01:02:30 -0400

    v1.2 hotfix 2

--- index.js
+++ index.js
@@ -720,7 +720,7 @@ client.on('message', message => {
     if (command == 'help') {
       var helpEmbed = new Discord.MessageEmbed()
         .setTitle('Commands')
-        .setDescription('[`' + prefix + 'prefix`](https://github.com/Bubblebyb/pk-bot/wiki/prefix) \n [`' + prefix + 'help`](https://github.com/Bubblebyb/pk-bot/wiki/Help): Gives a list of commands \n [`' + prefix + 'params`](https://github.com/Bubblebyb/pk-bot/wiki/Parameters): Sets a filter and selects bonuses for a pk \n [`' + prefix + 'pk`](https://github.com/Bubblebyb/pk-bot/wiki/pk): Starts the pk \n [`' + prefix + 'skip`](https://github.com/Bubblebyb/pk-bot/wiki/skip): Skips to the next question \n [`' + prefix + 'end`](https://github.com/Bubblebyb/pk-bot/wiki/End): Ends the pk')
+        .setDescription('[`' + prefix + 'help`](https://github.com/Bubblebyb/pk-bot/wiki/Help): Gives a list of commands \n [`' + prefix + 'prefix`](https://github.com/Bubblebyb/pk-bot/wiki/prefix): Displays or changes the current prefix \n  [`' + prefix + 'params`](https://github.com/Bubblebyb/pk-bot/wiki/Parameters): Sets a filter and selects bonuses for a pk \n [`' + prefix + 'pk`](https://github.com/Bubblebyb/pk-bot/wiki/pk): Starts the pk \n [`' + prefix + 'skip`](https://github.com/Bubblebyb/pk-bot/wiki/skip): Skips to the next question \n [`' + prefix + 'end`](https://github.com/Bubblebyb/pk-bot/wiki/End): Ends the pk')
         .setFooter('Click on any of the commands to go to their wiki page')
       message.channel.send(helpEmbed);
     }
